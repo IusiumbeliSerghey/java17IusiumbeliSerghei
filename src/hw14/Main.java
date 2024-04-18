ebf8ee2a963a2aa8c38caaa5c1e485e54a3751f8
IusiumbeliSerghey <sincework04@mail.ru>
2024-04-18 22:48:54 +0300

    hw14

--- src/hw14/Main.java
+++ src/hw14/Main.java
@@ -1,22 +1,32 @@
 package hw14;
+//Добавить в свои классы проверку и выкидывать исключения в случае ошибок
+//Обработать исключения в try catch
 
 
 public class Main {
     public static int findMax(int[] array) {
-        if (array.length == 0) {
-            throw new IllegalArgumentException("Array must not be empty");
-        }
-
-        int max = array[0]; // Инициализация первым элементом
-
-        for (int i = 1; i < array.length; i++) {
-            if (array[i] > max) {
-                max = array[i];
+        try {
+            if (array.length == 0) {
+                throw new IllegalArgumentException("Array must not be empty");
             }
-        }
 
-        return max;
+            int max = array[0]; // Инициализация первым элементом
+
+            for (int i = 1; i < array.length; i++) {
+                if (array[i] > max) {
+                    max = array[i];
+                }
+            }
+
+            return max;
+        } catch (IllegalArgumentException e) {
+            System.err.println("Error: " + e.getMessage());
+            return array.length;
+        }
     }
 
 
 }
+
+
+
