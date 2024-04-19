e02341567e56752f126eaad3747aeb7f5a4270d8
IusiumbeliSerghey <sincework04@mail.ru>
2024-04-19 18:51:26 +0300

    hw14

--- src/hw14/Exception.java
+++ src/hw14/Exception.java
@@ -2,8 +2,8 @@ package hw14;
 
 public class Exception extends Main {
     public static void main(String[] args) {
-    int[] array = {};
-    int max = findMax(array);
-    System.out.println("Максимальный элемент в массиве: " + max);
-}
+        int[] array = {};
+        int max = findMax(array);
+        System.out.println("Максимальный элемент в массиве: " + max);
+    }
 }
