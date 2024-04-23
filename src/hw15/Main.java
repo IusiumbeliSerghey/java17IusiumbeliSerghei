fe429cff298c6134e439d6126fd49b7941c83e84
IusiumbeliSerghey <sincework04@mail.ru>
2024-04-23 12:26:11 +0300

    hw15

--- src/hw15/Main.java
+++ src/hw15/Main.java
@@ -1,18 +1,16 @@
 package hw15;
+
 public class Main {
     public static void main(String[] args) {
-        Car myCar = new Car("Toyota", "Red", 0);
+        Car myCar = new Car("Toyota", "Red", 50);
 
         try {
             // Test drive with zero fuel consumption
-            myCar.drive(8);
+            myCar.safetyBelt("Not Belt");
+            myCar.drive(0);
+
         } catch (NotFuelException e) {
             System.err.println("Exception caught: " + e.getMessage());
-        }
-
-        try {
-            // Test safety belt with "Not Belt" status
-            myCar.safetyBelt("Not Belt");
         } catch (NotBeltException e) {
             System.err.println("Exception caught: " + e.getMessage());
         }
